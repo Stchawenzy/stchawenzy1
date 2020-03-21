053be3917b1f6af1622ce9befdc31f8e1a1e1711
Stchawenzy <48480710+Stchawenzy@users.noreply.github.com>
2020-03-21 20:39:02 +0100

    ReadMe
    JavaScript

---
+++ JavaScript_Task1/home.js
@@ -0,0 +1,17 @@
+//Store your name in a Variable
+var name = "Stchawenzy";
+console.log(name);
+
+//Store your courses in an array
+let courses = ["html", "css", "javascript", "nodejs"];
+console.log(courses);
+
+//Display Even numbers from 1-200(inclusive)
+const num = [];
+
+for (let i=0; i<=200; i++) {
+    if (i%2===0) {
+        num.push(i);
+    }
+}
+console.log(num);
